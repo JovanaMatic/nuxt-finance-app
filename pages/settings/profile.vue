5e18ec1cfdd57493a8f312ef52b22fbe5227968e
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-01-18 20:12:30 +0100

    Update profile logic implementation

--- pages/settings/profile.vue
+++ pages/settings/profile.vue
@@ -1,10 +1,10 @@
 <template>
-  <UForm :schema="schema" :state="state">
+  <UForm :schema="schema" :state="state" @submit="saveProfile">
     <UFormGroup label="Full name" class="mb-4" name="name">
       <UInput v-model="state.name"></UInput>
     </UFormGroup>
 
-    <UFormGroup label="Email" class="mb-4" name="email" :peding="isLoading" help="You will receive a confirmation email on both old and new email address">
+    <UFormGroup label="Email" class="mb-4" name="email" :loading="isLoading" :disabled="isLoading" help="You will receive a confirmation email on both old and new email address">
       <UInput v-model="state.email"></UInput>
     </UFormGroup>
 
@@ -21,7 +21,7 @@
   const isLoading = ref(false)
 
   const state = ref({
-    name: '',
+    name: user.value.user_metadata?.full_name,
     email: user.value.email
   })
 
@@ -29,6 +29,36 @@
     name: z.string().min(2).optional(),
     email: z.string().email()
   })
+
+  const saveProfile = async () => {
+    isLoading.value = true
+    try {
+      const data = {
+        full_name: state.value.name
+      }
+      const { error } = await supabase.auth.updateUser(data)
+
+      if(state.value.email !== user.value.email) {
+        data.email = state.value.email
+      }
+      console.log(data)
+
+      if (error) throw error
+
+      toastSuccess({
+        title: 'Profile updated',
+        description: 'Your profile has been updated successfully'
+      })
+    }
+    catch (error) {
+      toastError({
+        title: 'Error updating profile',
+        description: error.message
+      })
+    } finally {
+      isLoading.value = false
+    }
+  }
 
 </script>
 
